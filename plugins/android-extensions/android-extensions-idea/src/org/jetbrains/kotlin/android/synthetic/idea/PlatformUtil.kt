tancongcong123/kotlin

Android Extensions: Support platform modules (KT-26169)

plugins/android-extensions/android-extensions-idea/src/org/jetbrains/kotlin/android/synthetic/idea/PlatformUtil.kt
@@ -1,0 +1,19 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.android.synthetic.idea
+
+import org.jetbrains.kotlin.analyzer.ModuleInfo
+import org.jetbrains.kotlin.idea.caches.project.ModuleSourceInfo
+import org.jetbrains.kotlin.idea.caches.project.PlatformModuleInfo
+import org.jetbrains.kotlin.resolve.jvm.platform.JvmPlatform
+
+internal fun ModuleInfo.findAndroidModuleInfo(): ModuleSourceInfo? {
+    return when (this) {
+        is ModuleSourceInfo -> this.takeIf { it.platform is JvmPlatform }
+        is PlatformModuleInfo -> this.containedModules.find { it.platform is JvmPlatform }
+        else -> null
+    }
+}

didot/android

(cherry picked from commit 280a3e3d26d3b1403bad9bd0767fc78d9d878ea1)

plugins/android-extensions/android-extensions-idea/src/org/jetbrains/kotlin/android/synthetic/idea/PlatformUtil.kt
@@ -1,0 +1,19 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.android.synthetic.idea
+
+import org.jetbrains.kotlin.analyzer.ModuleInfo
+import org.jetbrains.kotlin.idea.caches.project.ModuleSourceInfo
+import org.jetbrains.kotlin.idea.caches.project.PlatformModuleInfo
+import org.jetbrains.kotlin.resolve.jvm.platform.JvmPlatform
+
+internal fun ModuleInfo.findAndroidModuleInfo(): ModuleSourceInfo? {
+    return when (this) {
+        is ModuleSourceInfo -> this.takeIf { it.platform is JvmPlatform }
+        is PlatformModuleInfo -> this.platformModule.takeIf { it.platform is JvmPlatform }
+        else -> null
+    }
+}