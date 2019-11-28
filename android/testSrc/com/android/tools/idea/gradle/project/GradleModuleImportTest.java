e58dcd85e5cb6981cf0911d4791274396fd8f319
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-11-28 18:00:32 +0300

    dispose: part 5
    GitOrigin-RevId: 4cec75da435972c8bf87cbd8848839b54ab3b018
    
    (cherry picked from commit 0519f4b6e0be27b3b00e24d0c5e8e460dc0fce8c)
    Change-Id: I8ac614170d555103408105b5997933c2b493e0bf

--- android/testSrc/com/android/tools/idea/gradle/project/GradleModuleImportTest.java
+++ android/testSrc/com/android/tools/idea/gradle/project/GradleModuleImportTest.java
@@ -40,7 +40,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ex.ProjectEx;
 import com.intellij.openapi.project.ex.ProjectManagerEx;
 import com.intellij.openapi.project.impl.ProjectManagerImpl;
-import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.ThrowableComputable;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.testFramework.PlatformTestUtil;
@@ -386,10 +385,9 @@ b/145809317 */
         ApplicationManager.getApplication().runWriteAction(new Runnable() {
           @Override
           public void run() {
-            Disposer.dispose(project);
             ProjectManagerEx projectManager = ProjectManagerEx.getInstanceEx();
+            projectManager.forceCloseProject(project);
             if (projectManager instanceof ProjectManagerImpl) {
-              projectManager.forceCloseProject(project);
               Collection<Project> projectsStillOpen = Arrays.asList(projectManager.getOpenProjects());
               if (!projectsStillOpen.isEmpty()) {
                 Project project = projectsStillOpen.iterator().next();
