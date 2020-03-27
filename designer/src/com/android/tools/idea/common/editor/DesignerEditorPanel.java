fd23cef5cbd9b07bdb5a40b4b7124eb89fa4f2f6
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2020-03-27 20:00:40 +0300

    IDEA-236203: Android: add missing progress indicator when IDEA downloads layoutlib-resources
    This is happening when Layout Editor opens first time in a newly installed IDE.
    
    GitOrigin-RevId: 9f14ea83aa3170db4d508c53e0c3c3aaa50cbcea
    
    (cherry picked from commit 3483cd00e5b28fd27ceaccd1813a60bb3c25aa95)
    Change-Id: I42308dcb29ca38ddaac81b296d864f7fe57c6e5e

--- designer/src/com/android/tools/idea/common/editor/DesignerEditorPanel.java
+++ designer/src/com/android/tools/idea/common/editor/DesignerEditorPanel.java
@@ -22,6 +22,7 @@ import com.android.tools.adtui.common.AdtPrimaryPanel;
 import com.android.tools.adtui.workbench.ToolWindowDefinition;
 import com.android.tools.adtui.workbench.WorkBench;
 import com.android.tools.idea.AndroidPsiUtils;
+import com.android.tools.idea.IdeInfo;
 import com.android.tools.idea.actions.DesignerDataKeys;
 import com.android.tools.idea.common.error.IssuePanelSplitter;
 import com.android.tools.idea.common.lint.ModelLintIssueAnnotator;
@@ -52,6 +53,7 @@ import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.project.ModuleListener;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
@@ -324,6 +326,13 @@ public class DesignerEditorPanel extends JPanel implements Disposable {
             Logger.getInstance(DesignerEditorPanel.class).info(cause.getMessage());
             SyncUtil.listenUntilNextSync(myProject, this, ignore -> initNeleModel());
             myWorkBench.loadingStopped("Design editor is unavailable until next gradle sync.");
+            return;
+          }
+
+          if (cause instanceof ProcessCanceledException){
+            // e.g. when IDEA user clicks 'cancel' button while required resources are downloaded from the Internet .
+            myWorkBench.loadingStopped("Failed to initialize editor (operation canceled).");
+            assert !IdeInfo.getInstance().isAndroidStudio(): "AndroidStudio has all the resources bundled with the IDE.";
             return;
           }
 
