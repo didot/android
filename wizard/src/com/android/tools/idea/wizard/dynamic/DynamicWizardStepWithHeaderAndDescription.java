5e53d367603967727e7e9ba8a8b914fce6cf8b7b
Iurii.Sukhanov <iurii.sukhanov@jetbrains.com>
2021-11-16 17:40:29 +0300

    [rp-android] Replace "Android Studio" with %ideName in strings for RIDER
    GitOrigin-RevId: 04aa2e41b06dd9006fe6b53eac101108dd39bddf
    (cherry picked from commit c86988523ecf34716a4825126e764ed25143f338)
    Change-Id: I80170b4a6cb2a4ca3817c5b54eec751af42987ce
    
    # Conflicts:
    #	android/src/org/jetbrains/android/util/AndroidBundle.java

--- wizard/src/com/android/tools/idea/wizard/dynamic/DynamicWizardStepWithHeaderAndDescription.java
+++ wizard/src/com/android/tools/idea/wizard/dynamic/DynamicWizardStepWithHeaderAndDescription.java
@@ -20,6 +20,7 @@ import static com.android.tools.idea.wizard.dynamic.ScopedStateStore.createKey;
 
 import com.android.tools.idea.wizard.WizardConstants;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.application.ApplicationNamesInfo;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBLabel;
@@ -130,7 +131,6 @@ public abstract class DynamicWizardStepWithHeaderAndDescription extends DynamicW
   }
 
   public static final class WizardStepHeaderSettings {
-    public static final String PRODUCT_DESCRIPTION = "Android Studio";
 
     @NotNull public final String title;
     @Nullable public final String description;
@@ -146,12 +146,12 @@ public abstract class DynamicWizardStepWithHeaderAndDescription extends DynamicW
 
     @NotNull
     public static WizardStepHeaderSettings createCustomColorHeader(@NotNull JBColor color, @NotNull String title) {
-      return new WizardStepHeaderSettings(title, PRODUCT_DESCRIPTION, null, color);
+      return new WizardStepHeaderSettings(title, ApplicationNamesInfo.getInstance().getProductName(), null, color);
     }
 
     @NotNull
     public static WizardStepHeaderSettings createProductHeader(@NotNull String title) {
-      return new WizardStepHeaderSettings(title, PRODUCT_DESCRIPTION, null, null);
+      return new WizardStepHeaderSettings(title, ApplicationNamesInfo.getInstance().getProductName(), null, null);
     }
 
     @NotNull
