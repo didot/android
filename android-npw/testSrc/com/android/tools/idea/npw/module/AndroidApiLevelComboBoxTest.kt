484fd68f23827c4c600737af255094de361aaade
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-01-09 12:48:42 +0300

    cleanup
    GitOrigin-RevId: dfe7511f2e44a0e535685d211f754f87fa1c825e
    (cherry picked from commit d4534aff20a875439a86a3890a6ac8dcdb2caa49)
    Change-Id: Ie38c320e52446c89ee82a43f8531f8a489500676

--- android-npw/testSrc/com/android/tools/idea/npw/module/AndroidApiLevelComboBoxTest.kt
+++ android-npw/testSrc/com/android/tools/idea/npw/module/AndroidApiLevelComboBoxTest.kt
@@ -52,7 +52,7 @@ class AndroidApiLevelComboBoxTest {
   fun testDefaultSelectedItem() {
     val formFactor = FormFactor.MOBILE
     assertEquals("none", PropertiesComponent.getInstance().getValue(getPropertiesComponentMinSdkKey(formFactor), "none"))
-    val items: MutableList<VersionItem> = Lists.newArrayList(
+    val items: MutableList<VersionItem> = mutableListOf(
       VersionItem.fromStableVersion(formFactor.defaultApi - 1),
       VersionItem.fromStableVersion(formFactor.defaultApi),
       // Default is at position 1
