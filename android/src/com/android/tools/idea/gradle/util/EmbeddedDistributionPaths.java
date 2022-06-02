60d0c992d07f2c5bd6c2487087cd126674bb6016
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2022-06-02 14:15:48 +0300

    Android: fix paths to deployable files in IDEA
    IDEA should not use AOSP debug paths. All the deployable files are downloaded dynamically, and have the same location in debug and release builds.
    
    GitOrigin-RevId: 335c9f00dd800ba3dd75cadc94024f3274e09085
    (cherry picked from commit 92a30b8631ef7b872ff6c6545422f814b934a7e8)
    Change-Id: I5387d525d96c3a78e1848c95304b9a4386d3896e

--- android/src/com/android/tools/idea/gradle/util/EmbeddedDistributionPaths.java
+++ android/src/com/android/tools/idea/gradle/util/EmbeddedDistributionPaths.java
@@ -116,9 +116,8 @@ public class EmbeddedDistributionPaths {
 
   public String findEmbeddedInstaller() {
     final String path = "plugins/android/resources/installer";
-    if (StudioPathManager.isRunningFromSources()) {
+    if (StudioPathManager.isRunningFromSources() && IdeInfo.getInstance().isAndroidStudio()) {
       // Development mode
-      assert IdeInfo.getInstance().isAndroidStudio(): "Bazel paths exist only in AndroidStudio development mode";
       return StudioPathManager.resolvePathFromSourcesRoot("bazel-bin/tools/base/deploy/installer/android-installer").toAbsolutePath().toString();
     } else {
       File file = getOptionalIjPath(path);
