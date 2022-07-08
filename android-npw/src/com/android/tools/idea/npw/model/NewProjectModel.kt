a521449775d9c7dea1f9346a01dbb3316c5388e2
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-07-08 17:26:29 +0300

    cleanup
    GitOrigin-RevId: 94191fca210560ae2f7ba02b4eeb9d6335ff5903
    (cherry picked from commit d37abae0edff84c8b05ba8fb6f0a87b05a0df217)
    Change-Id: Ib88164778534678b33f2a81bdca8dbce6aca684e

--- android-npw/src/com/android/tools/idea/npw/model/NewProjectModel.kt
+++ android-npw/src/com/android/tools/idea/npw/model/NewProjectModel.kt
@@ -119,7 +119,11 @@ class NewProjectModel : WizardModel(), ProjectModelData {
           }
         }
 
-        val openProjectTask = OpenProjectTask(project = newProject, isNewProject = false, forceOpenInNewFrame = true)
+        val openProjectTask = OpenProjectTask {
+          this.project = project
+          isNewProject = false  // We have already created a new project.
+          forceOpenInNewFrame = true
+        }
         ProjectManagerEx.getInstanceEx().openProject(projectBaseDirectory.toPath(), openProjectTask)
       }
     }.queue()
