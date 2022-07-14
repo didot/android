835963c6d3c15556193c641f70391bfa296847ae
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-07-14 10:17:58 +0300

    drop EdtInvocationManager.dispatchAllInvocationEvents
    GitOrigin-RevId: 49567e0e12121e46154206709b52fd06fa732845
    (cherry picked from commit 3a1abe7c0f15a517e90ae53f5c7c6b8d679ee83a)
    Change-Id: Ib4b75836994de7a014395b05e6557339f18a6cb3

--- android/testSrc/com/android/tools/idea/ddms/screenshot/ScreenshotViewerTest.kt
+++ android/testSrc/com/android/tools/idea/ddms/screenshot/ScreenshotViewerTest.kt
@@ -28,7 +28,7 @@ import com.intellij.openapi.ui.DialogWrapper.CLOSE_EXIT_CODE
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.testFramework.PlatformTestUtil
 import com.intellij.testFramework.RunsInEdt
-import com.intellij.util.ui.EdtInvocationManager.dispatchAllInvocationEvents
+import com.intellij.util.ui.EDT.dispatchAllInvocationEvents
 import org.intellij.images.ui.ImageComponent
 import org.intellij.images.ui.ImageComponentDecorator
 import org.junit.After
