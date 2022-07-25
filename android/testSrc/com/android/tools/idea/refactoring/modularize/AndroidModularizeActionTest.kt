5cc92a08322d13b70400675e35b552776ba5020b
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2022-07-25 15:36:23 +0300

    Fix wrong imports
    Change-Id: I386696ab3e65995b6ac06f4237c0895d3d429b5b

--- android/testSrc/com/android/tools/idea/refactoring/modularize/AndroidModularizeActionTest.kt
+++ android/testSrc/com/android/tools/idea/refactoring/modularize/AndroidModularizeActionTest.kt
@@ -31,8 +31,8 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.refactoring.actions.BaseRefactoringAction
 import org.jetbrains.android.util.AndroidUtils
+import org.jetbrains.kotlin.base.util.letIf
 import org.jetbrains.kotlin.idea.KotlinFileType
-import org.jetbrains.kotlin.miniStdLib.letIf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.experimental.runners.Enclosed
