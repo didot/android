e2556d9c53208dc527e4d4259edfa50a6df0120c
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2022-07-25 22:15:38 +0300

    Addendum to "drop EdtInvocationManager.dispatchAllInvocationEvents Vladimir Krivosheev* 14.07.2022, 10:17"
    Change-Id: Ica9627ffbbcbfcff5f262bc14be56cccc05351a4

--- android-adb-ui/testSrc/com/android/tools/idea/ui/screenshot/ScreenshotViewerTest.kt
+++ android-adb-ui/testSrc/com/android/tools/idea/ui/screenshot/ScreenshotViewerTest.kt
@@ -29,7 +29,7 @@ import com.intellij.testFramework.PlatformTestUtil
 import com.intellij.testFramework.ProjectRule
 import com.intellij.testFramework.RuleChain
 import com.intellij.testFramework.RunsInEdt
-import com.intellij.util.ui.EdtInvocationManager.dispatchAllInvocationEvents
+import com.intellij.util.ui.EDT.dispatchAllInvocationEvents
 import org.intellij.images.ui.ImageComponent
 import org.intellij.images.ui.ImageComponentDecorator
 import org.junit.After
