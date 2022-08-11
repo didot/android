512299885819f7be67ce25241b285209805b380f
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2022-08-11 11:49:14 +0300

    AUTO: getWorkspaceRoot().resolve($param$) => com.android.testutils.TestUtils.resolveWorkspacePath($param$) (Kotlin)
    Change-Id: I1d2e7fe9076441243358f462eb6b7a21e8c855e8

--- android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
+++ android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
@@ -25,7 +25,6 @@ import com.android.sdklib.AndroidVersion
 import com.android.sdklib.devices.Abi
 import com.android.testutils.TestUtils.getLatestAndroidPlatform
 import com.android.testutils.TestUtils.getSdk
-import com.android.testutils.TestUtils.getWorkspaceRoot
 import com.android.tools.idea.gradle.LibraryFilePaths
 import com.android.tools.idea.gradle.model.IdeAaptOptions
 import com.android.tools.idea.gradle.model.IdeAndroidProjectType
@@ -1770,7 +1769,7 @@ interface GradleIntegrationTest {
    */
   @JvmDefault
   fun resolveTestDataPath(testDataPath: @SystemIndependent String): File {
-    val testDataDirectory = getWorkspaceRoot().resolve(toSystemDependentName(getTestDataDirectoryWorkspaceRelativePath()))
+    val testDataDirectory = com.android.testutils.TestUtils.resolveWorkspacePath(toSystemDependentName(getTestDataDirectoryWorkspaceRelativePath()))
     return testDataDirectory.resolve(toSystemDependentName(testDataPath)).toFile()
   }
 
