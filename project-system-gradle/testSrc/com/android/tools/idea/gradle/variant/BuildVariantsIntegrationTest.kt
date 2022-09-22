msft-mirror-aosp/platform.tools.adt.idea

Support build name prefixed gradle paths in tests
(1) Do not use GradleFacet to find modules
(2) Add missing root modules to light sync test projects as they
    are always supposed to exist.

Bug: 243886833
Test: n/a
Change-Id: I5bcdc14e59b9039e835701b6602104080828ea96

project-system-gradle/testSrc/com/android/tools/idea/gradle/variant/BuildVariantsIntegrationTest.kt
@@ -371,7 +371,7 @@ class BuildVariantsIntegrationTest {
       switchVariant(project, ":app", "release")
       expect.consistentConfigurationOf(project)
       expect.thatModuleVariantIs(project, ":app", "release")
-      expect.thatModuleVariantIs(project, "TestCompositeLib1:lib", "release")
+      expect.thatModuleVariantIs(project, ":includedLib1:lib", "release")
     }
   }
 

didot/android

project-system-gradle/testSrc/com/android/tools/idea/gradle/variant/BuildVariantsIntegrationTest.kt
@@ -365,7 +365,7 @@ class BuildVariantsIntegrationTest : GradleIntegrationTest {
       switchVariant(project, ":app", "release")
       expect.consistentConfigurationOf(project)
       expect.thatModuleVariantIs(project, ":app", "release")
-      expect.thatModuleVariantIs(project, "TestCompositeLib1:lib", "release")
+      expect.thatModuleVariantIs(project, ":TestCompositeLib1:lib", "release")
     }
   }
 
@@ -676,7 +676,7 @@ private fun Expect.consistentConfigurationOf(project: Project) {
 
 private fun Expect.thatModuleVariantIs(project: Project, gradlePath: String, variant: String, abi: String? = null) {
   val module = project.gradleModule(gradlePath)
-  withMessage("Selected variant in AndroidModuleModel $gradlePath").that(module?.selectedModelVariant()).isEqualTo(variant)
+  withMessage("Selected variant in AndroidModuleModel $module $gradlePath").that(module?.selectedModelVariant()).isEqualTo(variant)
   withMessage("Selected variant in AndroidFacet $gradlePath").that(module?.selectedFacetVariant()).isEqualTo(variant)
   if (abi != null) {
     withMessage("Selected variant in NdkModuleModel $gradlePath")