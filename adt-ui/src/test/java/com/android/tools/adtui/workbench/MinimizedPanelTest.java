873c799fbb784a16a1342461a774bfef48f8a071
andrei.kuznetsov <andrei.kuznetsov@jetbrains.com>
2022-10-08 00:03:38 +0200

    AUTO: org.mockito.Matchers.eq => org.mockito.Mockito.eq (Text)
    Change-Id: Iacc50493c31b52fdf398e33879f315a4d3b36efe

--- adt-ui/src/test/java/com/android/tools/adtui/workbench/MinimizedPanelTest.java
+++ adt-ui/src/test/java/com/android/tools/adtui/workbench/MinimizedPanelTest.java
@@ -16,9 +16,9 @@
 package com.android.tools.adtui.workbench;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
