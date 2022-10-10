527ca9378bdb5c1b3def41d53dd0accde0371cdc
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2022-10-10 15:36:39 +0200

    introducing AsyncProjectViewPane to replace deprecated AbstractProjectViewPSIPane
    GitOrigin-RevId: 7c41c51a1c2d75a96a45cb9567b4d92fc4a92921
    (cherry picked from commit 23ebc7742b36757b53b2b01803f9b4cc2d6627c4)

--- android-navigator/src/com/android/tools/idea/navigator/AndroidProjectViewPane.java
+++ android-navigator/src/com/android/tools/idea/navigator/AndroidProjectViewPane.java
@@ -35,20 +35,17 @@ import com.intellij.facet.ProjectWideFacetListenersRegistry;
 import com.intellij.ide.DeleteProvider;
 import com.intellij.ide.SelectInTarget;
 import com.intellij.ide.impl.ProjectViewSelectInTarget;
-import com.intellij.ide.projectView.BaseProjectTreeBuilder;
 import com.intellij.ide.projectView.ProjectView;
 import com.intellij.ide.projectView.ProjectViewSettings;
 import com.intellij.ide.projectView.TreeStructureProvider;
 import com.intellij.ide.projectView.ViewSettings;
-import com.intellij.ide.projectView.impl.AbstractProjectViewPSIPane;
 import com.intellij.ide.projectView.impl.AbstractProjectViewPane;
+import com.intellij.ide.projectView.impl.AsyncProjectViewPane;
 import com.intellij.ide.projectView.impl.ProjectAbstractTreeStructureBase;
 import com.intellij.ide.projectView.impl.ProjectTreeStructure;
 import com.intellij.ide.projectView.impl.ProjectViewTree;
 import com.intellij.ide.projectView.impl.nodes.PackageElement;
-import com.intellij.ide.util.treeView.AbstractTreeBuilder;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
-import com.intellij.ide.util.treeView.AbstractTreeUpdater;
 import com.intellij.ide.util.treeView.NodeDescriptor;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.DataContext;
@@ -80,7 +77,7 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class AndroidProjectViewPane extends AbstractProjectViewPSIPane {
+public class AndroidProjectViewPane extends AsyncProjectViewPane {
   // Note: This value is duplicated in ProjectViewImpl.java to set the default view to be the Android project view.
   public static final String ID = AndroidProjectView.ID;
 
@@ -181,24 +178,10 @@ public class AndroidProjectViewPane extends AbstractProjectViewPSIPane {
     return new AndroidProjectTreeStructure(myProject, ID);
   }
 
-  @Nullable
-  @Override
-  protected BaseProjectTreeBuilder createBuilder(@NotNull DefaultTreeModel treeModel) {
-    return null;
-  }
-
   @NotNull
   @Override
   protected ProjectViewTree createTree(@NotNull final DefaultTreeModel treeModel) {
     return new MyProjectViewTree(treeModel);
-  }
-
-  @NotNull
-  @Override
-  protected AbstractTreeUpdater createTreeUpdater(@NotNull AbstractTreeBuilder treeBuilder) {
-    return new AbstractTreeUpdater(treeBuilder) {
-      // unique class to simplify search through the logs
-    };
   }
 
   @Override
